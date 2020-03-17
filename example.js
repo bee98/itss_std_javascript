4c6ad6cdf8e15c87ee7c84d81d0bfdaae04c27c6
bee98 <43488353+bee98@users.noreply.github.com>
2020-03-17 13:00:16 +0700

    課題2

--- example.js
+++ example.js
@@ -3,3 +3,16 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+
+let number;
+let arr = [];
+
+arr = process.argv.slice(2);
+let arrN = arr.map((value,i)=>Number(value));
+arrN.sort((a, b) => {
+	if(a > b) return 1;
+	else if(a < b) return -1;
+	else return 0;
+});
+
+console.log(arrN);
