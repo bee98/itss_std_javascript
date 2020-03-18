9ef49574aa8928bfda708e866d82b578e6881f97
bee98 <43488353+bee98@users.noreply.github.com>
2020-03-18 22:15:12 +0700

    課題4

--- example.js
+++ example.js
@@ -3,10 +3,15 @@
 use strict';
 
 // このファイルを修正して、プルリクエストしてください。
-let arr = [];
-arr = process.argv.slice(2);
-let arrN = arr.map((element)=>Number(element));
-const max = Math.max(...arrN);
-const min = Math.min(...arrN);
-const value  = (min+max)/2;
-console.log(Math.floor(value));
+// let arr = [];
+// arr = process.argv.slice(2);
+// let arrN = arr.map((element)=>Number(element));
+// const max = Math.max(...arrN);
+// const min = Math.min(...arrN);
+// const value  = (min+max)/2;
+// console.log(Math.floor(value
+const arr = process.argv.slice(2);
+const startDate = new Date(arr[0],arr[1],arr[2]);
+const endDate   = new Date(arr[3],arr[4],arr[5]);
+const days = (endDate.getTime() - startDate.getTime()) / (1000*60*60*24);
+console.log(Math.floor(days));
