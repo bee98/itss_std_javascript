017ce8f4deb9e19e86ad21c0ea32b7ee392e4f03
bee98 <43488353+bee98@users.noreply.github.com>
2020-03-18 20:32:38 +0700

    課題6

--- example.js
+++ example.js
@@ -1,18 +1,12 @@
 #!/usr/bin/env node
 
-'use strict';
+use strict';
 
 // このファイルを修正して、プルリクエストしてください。
-
-let number;
 let arr = [];
-
 arr = process.argv.slice(2);
-let arrN = arr.map((value,i)=>Number(value));
-arrN.sort((a, b) => {
-	if(a > b) return 1;
-	else if(a < b) return -1;
-	else return 0;
-});
-
-console.log(arrN);
+let arrN = arr.map((element)=>Number(element));
+const max = Math.max(...arrN);
+const min = Math.min(...arrN);
+const value  = (min+max)/2;
+console.log(Math.floor(value));
